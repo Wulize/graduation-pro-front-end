72cba4e6237a1c81e5473b6ed6b94ccf9e318d9a
unknown <Wulize@172105861@qq.com>
2021-12-15 14:05:26 +0800

    创建前端项目文件

---
+++ src/components/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <div class="name">{{ msg }}</div>
+    <div class="password"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class Login extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.name {
+  width: 20%;
+  height: 2rem;
+}
+.password {
+  width: 20%;
+  height: 2rem;
+}
+</style>
