cf2ac7b3db55bdbd4f52ffac1ca66eb9e0bbb812
unknown <Wulize@172105861@qq.com>
2021-12-19 18:31:28 +0800

    feat：注册登录接口测试

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,8 +2,16 @@
   <div class="login-wrapper">
     <el-input v-model="userEmail" placeholder="请输入您的邮箱"></el-input>
     <el-input v-model="userName" placeholder="请输入您的用户名"></el-input>
-    <el-input v-model="userCode1" placeholder="请输入您的密码" showPassword></el-input>
-    <el-input v-model="userCode2" placeholder="请再次输入您的密码" showPassword></el-input>
+    <el-input
+      v-model="userCode1"
+      placeholder="请输入您的密码"
+      showPassword
+    ></el-input>
+    <el-input
+      v-model="userCode2"
+      placeholder="请再次输入您的密码"
+      showPassword
+    ></el-input>
     <el-input v-model="userValidation" placeholder="请输入验证码"></el-input>
     <el-button type="primary">确定</el-button>
     <el-button type="primary" @click="getValidation">获取验证码</el-button>
@@ -11,23 +19,28 @@
 </template>
 
 <script>
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 @Component
 export default class Register extends Vue {
-  userName = ''
-  userEmail = ''
-  userCode1 = ''
-  userCode2 = ''
-  userValidation = ''
+  userName = "";
+  userEmail = "";
+  userCode1 = "";
+  userCode2 = "";
+  userValidation = "";
 
   getValidation() {
-    console.log('等待验证码')
+    console.log("等待验证码");
     axios({
-      url: 'localhost:3000/users/email',
-      method: 'get',
+      url: "localhost:3000/users/email",
+      method: "get",
+      params: {
+        user_name: this.userName,
+        email: this.userEmail,
+      },
     }).then((code) => {
-      alert(code)
-    })
+      alert(code);
+    });
   }
 }
 </script>
