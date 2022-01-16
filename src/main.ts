edab74b1136dacbf53eff443fdaa0e3e0416fb55
unknown <Wulize@172105861@qq.com>
2022-01-16 21:56:44 +0800

    feat:新增语音消息发送功能02

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Http from '@/utils'
-import { blobToBase64, base64ToBlob } from "@/utils/blobToBase64";
+import { base64ToBlob } from "@/utils/blobToBase64";
 
 Vue.config.productionTip = false
 Vue.prototype.$http = new Http()
